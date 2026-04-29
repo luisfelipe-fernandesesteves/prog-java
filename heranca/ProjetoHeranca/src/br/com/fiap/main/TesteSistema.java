c1e2efb2d2af5a61ffe9bc7ace73aa083338d221
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-04-29 09:44:53 -0300

    classes importadas

--- heranca/ProjetoHeranca/src/br/com/fiap/main/TesteSistema.java
+++ heranca/ProjetoHeranca/src/br/com/fiap/main/TesteSistema.java
@@ -1,4 +1,11 @@
 package br.com.fiap.main;
 
+import br.com.fiap.entities.Pessoa;
+import br.com.fiap.entities.PessoaJuridica;
+import br.com.fiap.entities.PessoaFisica;
+import br.com.fiap.entities.DadosBancarios;
+
+import javax.swing.*;
+
 public class TesteSistema {
 }
