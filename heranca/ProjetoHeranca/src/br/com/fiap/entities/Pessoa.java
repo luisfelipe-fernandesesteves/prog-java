df3f770f30f860b4f5f84c93a4e64244d162297f
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-04-29 08:42:45 -0300

    metodo constructor, gertter & setter e to string adicionado para a super classe (Pessoa)

--- heranca/ProjetoHeranca/src/br/com/fiap/entities/Pessoa.java
+++ heranca/ProjetoHeranca/src/br/com/fiap/entities/Pessoa.java
@@ -7,4 +7,55 @@ public class Pessoa {
     protected String fone;
     protected double renda;
 
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String email, String fone, double renda) {
+        this.nome = nome;
+        this.email = email;
+        this.fone = fone;
+        this.renda = renda;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", fone='" + fone + '\'' +
+                ", renda=" + renda +
+                '}';
+    }
 }
