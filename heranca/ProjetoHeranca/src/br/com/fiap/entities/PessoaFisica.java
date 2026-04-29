6ff58019171850a36149fc8b5d2f68cae0845650
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-04-29 08:50:37 -0300

    atributos adicionados

--- heranca/ProjetoHeranca/src/br/com/fiap/entities/PessoaFisica.java
+++ heranca/ProjetoHeranca/src/br/com/fiap/entities/PessoaFisica.java
@@ -1,4 +1,10 @@
 package br.com.fiap.entities;
 
 public class PessoaFisica {
+
+    private String cpf;
+    private String rg;
+    private String dataNascimento;
+    private DadosBancarios dadosBancarios; //atributo de referencia
+
 }
